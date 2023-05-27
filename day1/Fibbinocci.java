48aae0bb8f56621e680193b299b259af3c827718
Vaishu144 <134801986+Vaishu144@users.noreply.github.com>
2023-05-27 22:55:52 +0530

    Add files via upload
    Day 1

---
+++ day1/Fibbinocci.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class Fibbinocci {
+
+	public static void main(String[] args) {
+		int firstNum = 0;
+		int secNum = 1;
+		int sum = 0;
+				System.out.println(firstNum);
+		for (int i=0; i<=11; i++)
+		{
+			sum = firstNum + secNum;
+			System.out.println(sum);
+			firstNum = SecNum;
+			SecNum = Sum;
+			
+		}
+			
+				
+				
+
+	}
+
+}
---
+++ day1/Tour.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Tour {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int noOfDays = 5;
+		String placeOfVisit = "Goa";
+		char group = 'V';
+		float time = 10.30f;
+		
+		System.out.println("Tour Plan");
+		System.out.println("No of Days" + ":" + noOfDays);
+		System.out.println("Place of Visit" + ":" + placeOfVisit);
+		System.out.println("Travel Group" + ":" +group);
+		System.out.println("Start Time" + ":" +time);
+
+	}
+
+}
